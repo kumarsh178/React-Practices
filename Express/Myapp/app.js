90ca6aca738be6fa115d6a4109f582236bf26d03
kumarsh178 <kumarsh178@gmail.com>
2024-07-26 08:23:49 +0000

    express code

--- Express/Myapp/app.js
+++ Express/Myapp/app.js
@@ -4,4 +4,13 @@ const app = express();
 app.get('/ping',(request,response) => {
  response.send("Pong here");
 });
+app.route('/users').get(function(request,response,next){
+    response.send(request.method);
+}).post(function(request,response,next){
+    response.send(request.method);
+}).put(function(request,response,next){
+    response.send(request.method);
+}).delete(function(request,response,next){
+    response.send(request.method);
+});
 app.listen(8080,'localhost');
